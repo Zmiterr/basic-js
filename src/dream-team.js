216e110f5fa7d05f864dc3ba299004816801f076
Zmiter <ozmiter@gmail.com>
2021-03-15 16:56:35 +0300

    feat: add solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,8 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
-  return Array.isArray(members)?members.flat(10).filter(e => typeof(e)=='string').map(function edStr(e){   
+  return Array.isArray(members)?members.filter(e => typeof e ==='string').map(function edStr(e){   
                                                                                     return e.trim().toUpperCase()[0] 
-
   }).sort().join(''):false
 };
