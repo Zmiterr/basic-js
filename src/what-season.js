511587921d6a942b43df68d2a749f22ee27c8349
Zmiter <ozmiter@gmail.com>
2021-03-15 17:50:03 +0300

    feat: add tmp solution

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason( date ) {
+	//if (Object.prototype.toString.call(date) === '[object Date]'){
+  //throw new CustomError('Not implemented');
+switch (date.getMonth()) {
+  case 8:
+  case 9:
+  case 10: 
+    return('autumn');
+    break;
+  case 2:
+  case 3:
+  case 4:    
+    return('spring');
+    break;
+  case 5:
+  case 6:
+  case 7:    
+    return('summer');
+    break;
+  case 11:
+  case 0:
+  case 1:    
+    return('winter');
+    break; 
+  default:
+    return('Unable to determine the time of year!');
+//}
+}
 };
